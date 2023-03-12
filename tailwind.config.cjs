784cf0bb356bdd8e4f0be0f320443e8e8a272bb9
Lorin Speybrouck <lorin.speybrouck@proximus.be>
2023-03-12 21:55:08 +0100

    Added pega colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          "primary": "#f9984c",
+        },
+        dark: {
+          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+          "primary": "#f9984c",
+        }
+      },
+    ],
+  },
   content: [
     './src/**/*.{html,js,svelte,ts}',
     "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
