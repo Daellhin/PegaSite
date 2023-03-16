f026226a9a86cbc20d0b84735ee968faeb5236e4
Lorin Speybrouck <lorin.speybrouck@proximus.be>
2023-03-16 22:01:07 +0100

    Added new page page

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,6 +1,7 @@
 <h1 class="text-2xl font-bold mb-2">Aanpassingen</h1>
 
-<div class="flex flex-col w-fit gap-2">
+<div class="flex w-fit gap-2">
   <a href="/articles/new" class="btn btn-primary normal-case">Nieuw bericht</a>
   <a href="/events/new" class="btn btn-primary normal-case">Nieuw event</a>
+  <a href="/pages/new" class="btn btn-primary normal-case">Nieuwe pagina</a>
 </div>
---
+++ src/routes/pages/new/+page.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import Dropzone from "$components/Dropzone.svelte";
+  import Editor from "cl-editor/src/Editor.svelte";
+
+  let title = "";
+  let content = "";
+  let uploadedImages: File[] = [];
+  let showPreview = false;
+
+  function togglePreview() {
+    showPreview = !showPreview;
+  }
+  async function createPreviewPage() {}
+  async function savePage() {}
+</script>
+
+{#if showPreview}
+  <!-- Article preview -->
+  {#await createPreviewPage()}
+    <div>Loadig</div>
+  {:then article}
+    <button class="btn btn-primary btn-xs normal-case" on:click={togglePreview}>
+      Sluit preview
+    </button>
+  {/await}
+{:else}
+  <!-- Article editor -->
+  <div class="flex flex-row gap-3 items-center mb-1">
+    <h1 class="text-2xl font-bold">Nieuwe pagina</h1>
+    <button class="btn btn-primary btn-xs normal-case" on:click={togglePreview}>
+      Toon preview
+    </button>
+  </div>
+  <form class="flex flex-col gap-2" on:submit={savePage}>
+    <div class="form-control w-full max-w-sm">
+      <label class="label" for="title">
+        <span class="label-text">Titel van pagina:</span>
+      </label>
+      <input
+        id="title"
+        type="text"
+        placeholder="Titel"
+        class="input input-bordered border-2"
+        bind:value={title}
+      />
+    </div>
+
+    <div class="form-control w-full max-w-sm">
+      <label class="label" for="dropzone-file">
+        <span class="label-text">Afbeeldingen:</span>
+      </label>
+      <Dropzone bind:files={uploadedImages} accept={"image/*"} />
+    </div>
+
+    <div class="form-control">
+      <label class="label" for="editor">
+        <span class="label-text">Inhoud van bericht:</span>
+      </label>
+      <Editor html={content} on:change={(evt) => (content = evt.detail)} />
+    </div>
+
+    <button class="btn btn-primary btn-md mt-2 max-w-sm"
+      >Bericht aanmaken</button
+    >
+  </form>
+{/if}
+
+<style lang="postcss">
+  @import "../../../css/cl-editor.postcss";
+  @import "../../../css/usercontent.postcss";
+  @import "../../../css/multiselect.postcss";
+</style>
