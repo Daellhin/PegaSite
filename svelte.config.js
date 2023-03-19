8b14532b5c39c7e6f8fde237e44b13e303946969
Lorin Speybrouck <lorin.speybrouck@proximus.be>
2023-03-19 22:24:38 +0100

    [Fix] Postcss styling not applied

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
 import nesting from 'postcss-nesting';
 import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: {
-			plugins: [nesting()]
-		}
-	}),
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			postcss: {
+				plugins: [nesting()]
+			}
+		})
+	],
 	onwarn: (warning, handler) => {
 		if (
 			warning.code === "css-unused-selector" ||
