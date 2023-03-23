e3e4eb87d88eed431771ff680143d54db47a3c14
Lorin Speybrouck <lorin.speybrouck@proximus.be>
2023-03-23 23:16:41 +0100

    Update and fix svelte-select

--- src/components/Records/form-components/FormControlCustomSelect.svelte
+++ src/components/Records/form-components/FormControlCustomSelect.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import IoIosArrowDown from "svelte-icons/io/IoIosArrowDown.svelte";
   import Select from "svelte-select";
 
   export let label: string;
@@ -6,8 +7,6 @@
   export let required = false;
   export let items = Array<any>();
   export let groupBy: (arg0: any) => any;
-
-  const indicatorIcon = `<svg class="indicatorIcon" enable-background="new 0 0 29 14" height="14px" id="Layer_1" version="1.1" viewBox="0 0 29 14" width="29px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><polygon fill="currentColor" points="0.15,0 14.5,14.35 28.85,0 "/></svg>`;
 
   $: selectId = label?.replace(/ /g, "");
 
@@ -28,11 +27,16 @@
   <Select
     {items}
     {groupBy}
-    showIndicator={true}
-    indicatorSvg={indicatorIcon}
     id={selectId}
     on:select={handleSelect}
-  />
+    class={"select select-bordered"}
+    showChevron
+    placeholder={"Kies"}
+  >
+    <div class="w-4 h-4 text-gray-500" slot="chevron-icon">
+      <IoIosArrowDown />
+    </div>
+  </Select>
 </div>
 
 <style lang="postcss">
@@ -42,45 +46,30 @@
       0 1px 2px -1px var(--tw-shadow-color);
 
     /* Select */
-    --height: 2rem;
-    --indicatorRight: 0.3rem;
-    --background: transparent;
+    --background: hsl(var(--b1) / var(--tw-bg-opacity));
     --border: 1px hsl(var(--bc) / 0.2) solid;
-    --borderRadius: var(--rounded-btn, 0.5rem);
-    --borderFocusColor: hsla(var(--bc) / 0.2);
-    --listShadow: var(--tw-ring-offset-shadow, 0 0 #0000),
+    --border-hover: 1px hsl(var(--bc) / 0.2) solid;
+    --list-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
       var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+    --font-size: 0.875rem;
 
     /* Dropdown */
-    --listBackground: hsl(var(--b2, var(--b1)) / 1);
-    --itemHoverBG: hsl(var(--bc) / 0.1);
-    --itemIsActiveBG: hsl(var(--p) / 1);
-    --itemIsActiveColor: hsl(var(--pc) / 1);
+    --list-background: hsl(var(--b2, var(--b1)) / 1);
+    --item-hover-bg: hsl(var(--bc) / 0.1);
+    --item-is-active-bg: hsl(var(--p) / 1);
+    --item-is-active-color: hsl(var(--pc) / 1);
+    --group-title-text-transform: capitalize;
+    --item-height: 2.1rem;
+    --item-line-height: 2.1rem;
   }
   :global {
-    .listContainer {
-      z-index: 11 !important;
-    }
-    .indicatorIcon {
-      width: 0.54rem;
-      color: #9ca3af !important;
-    }
-    .selectContainer.focused {
+    .svelte-select.focused {
       outline: 2px solid hsla(var(--bc) / 0.2) !important;
       outline-offset: 2px !important;
     }
-    .selectContainer {
-      height: 3rem !important;
-    }
-    .selectContainer input,
-    .form-control {
-      cursor: text !important;
-      .indicatorIcon {
-        cursor: pointer !important;
-      }
-    }
-    .listItem .item {
-      font-size: 1em !important;
+    .svelte-select,
+    .svelte-select input {
+      cursor: pointer !important;
     }
   }
 </style>
