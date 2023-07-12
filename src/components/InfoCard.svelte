eb4374eea40010025b4df6593ad48b6cdd419d53
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2023-07-12 18:54:51 +0200

    Moved info card to seperate component

---
+++ src/components/InfoCard.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import MdInfoOutline from "svelte-icons/md/MdInfoOutline.svelte";
+</script>
+
+<div class="alert shadow-md w-fit">
+  <div class="text-info flex-shrink-0 w-6 h-6"><MdInfoOutline /></div>
+  <span>
+    <slot />
+  </span>
+</div>
