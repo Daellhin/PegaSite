b3c8acf835f74fac11137efef3c8af636cd1c1d2
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2023-07-13 13:15:22 +0200

    Added validator prop to FormControlText

--- src/components/formHelpers/FormControlText.svelte
+++ src/components/formHelpers/FormControlText.svelte
@@ -3,11 +3,15 @@
   export let value: string;
   export let required = false;
   export let size: "sm" | "xs" = "sm";
-  
+
   export let labelClass = "";
   export let placeholder: string;
+  export let validator: (value: string) => string | undefined = () => "";
+
+  let edited = false;
 
   $: inputId = label?.replace(/[ :]/g, "").toLowerCase();
+  $: error = validator(value);
 </script>
 
 <div
@@ -30,5 +34,13 @@
     {placeholder}
     class="input input-bordered border-2 w-full"
     bind:value
+    on:focusout={() => (edited = true)}
   />
+  {#if error && edited}
+    <label class="label" for={inputId}>
+      <span class={`label-text alt text-error ${labelClass}`}>
+        {error}
+      </span>
+    </label>
+  {/if}
 </div>
