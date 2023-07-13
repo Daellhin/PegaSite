74a1f289b18e8d025ad15f90c8e15de76b8663ab
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2023-07-13 13:16:29 +0200

    Added class prop to infocard

--- src/components/InfoCard.svelte
+++ src/components/InfoCard.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import MdInfoOutline from "svelte-icons/md/MdInfoOutline.svelte";
+
+  let className = "";
+  export { className as class };
 </script>
 
-<div class="alert shadow-md w-fit">
+<div class={"alert shadow-md w-fit " + className}>
   <div class="text-info flex-shrink-0 w-6 h-6"><MdInfoOutline /></div>
   <span>
     <slot />
