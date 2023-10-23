b87015f03c684e21fe5c594e80bfbebb1164abc3
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2023-10-23 23:03:07 +0200

    Add warning for duplicate orders

--- src/lib/domain/Link.ts
+++ src/lib/domain/Link.ts
@@ -2,24 +2,24 @@ import { capitalizeFirstLetter } from "$lib/utils/String"
 import type { DragableItem } from "$lib/utils/Types"
 
 export interface LinkJson {
-    title: string
-    order: number
-    customUrl?: string
+	title: string
+	order: number
+	customUrl?: string
 }
 export class Link {
-    constructor(
-        public title: string,
-        public order: number,
-        public customUrl?: string
-    ) { }
+	constructor(
+		public title: string,
+		public order: number,
+		public customUrl?: string
+	) { }
 
-    getUrl(edit = false) {
-        return this.customUrl || Link.normaliseUrl(this.title, edit)
-    }
+	getUrl(edit = false) {
+		return this.customUrl || Link.normaliseUrl(this.title, edit)
+	}
 
-    getId() {
-        return Link.normaliseId(this.title)
-    }
+	getId() {
+		return Link.normaliseId(this.title)
+	}
 
 	toDragableDragableItem() {
 		return {
@@ -28,51 +28,51 @@ export class Link {
 		} as DragableItem<Link>
 	}
 
-    toFirebaseJson() {
-        return {
-            order: this.order, ...this.customUrl && { customUrl: this.customUrl }
-        }
-    }
+	toFirebaseJson() {
+		return {
+			order: this.order, ...this.customUrl && { customUrl: this.customUrl }
+		}
+	}
 
-    static normaliseUrl(url: string, edit = false) {
-        return `/pages/${edit ? "edit/" : ""}${Link.normaliseId(url)}`
-    }
+	static normaliseUrl(url: string, edit = false) {
+		return `/pages/${edit ? "edit/" : ""}${Link.normaliseId(url)}`
+	}
 
-    static normaliseId(id: string) {
-        return id.trim().replace(/ /g, "-").toLowerCase()
-    }
-    static titleFromId(id:string) {
-        return capitalizeFirstLetter(id.replace(/-/g, ""))
-    }
+	static normaliseId(id: string) {
+		return id.trim().replace(/ /g, "-").toLowerCase()
+	}
+	static titleFromId(id: string) {
+		return capitalizeFirstLetter(id.replace(/-/g, ""))
+	}
 
-    static fromJson(json: LinkJson) {
-        return new Link(
-            json.title,
-            json.order,
-            json.customUrl
-        )
-    }
+	static fromJson(json: LinkJson) {
+		return new Link(
+			json.title,
+			json.order,
+			json.customUrl
+		)
+	}
 }
 
 export interface LinkGroupJson {
-    name: string
-    links: LinkJson[]
-    order: number
+	name: string
+	links: LinkJson[]
+	order: number
 }
 export class LinkGroup {
-    constructor(
-        public name: string,
-        public links: Link[],
-        public order: number
-    ) { }
+	constructor(
+		public name: string,
+		public links: Link[],
+		public order: number
+	) { }
 
-    static fromJson(json: LinkGroupJson) {
-        return new LinkGroup(
-            json.name,
-            json.links.map(Link.fromJson),
-            json.order
-        )
-    }
+	static fromJson(json: LinkGroupJson) {
+		return new LinkGroup(
+			json.name,
+			json.links.map(Link.fromJson),
+			json.order
+		)
+	}
 
 	toDragableDragableItem() {
 		return {
@@ -81,28 +81,34 @@ export class LinkGroup {
 		} as DragableItem<LinkGroup>
 	}
 
-    toFirebaseJson() {
-        const linkMap = this.links.map(link => [link.title, link.toFirebaseJson()])
-        return {
-            [this.name]: {
-                order: this.order,
-                links: Object.fromEntries(linkMap)
-            }
-        }
-    }
+	toFirebaseJson() {
+		const linkMap = this.links.map(link => [link.title, link.toFirebaseJson()])
+		return {
+			[this.name]: {
+				order: this.order,
+				links: Object.fromEntries(linkMap)
+			}
+		}
+	}
 
-    static fromFirebaseData(toMap: any) {
-        const links = Object.keys(toMap).flatMap((groupName) => {
-            const links = Object.keys(toMap[groupName].links).map((linkTitle) => {
-                const linkJson = toMap[groupName].links[linkTitle]
-                return new Link(linkTitle, linkJson.order, linkJson.customUrl)
-            }).sort((a, b) => a.order - b.order)
-            return new LinkGroup(groupName, links, toMap[groupName].order)
-        })
-        return links.sort((a, b) => a.order - b.order)
-    }
+	static fromFirebaseData(toMap: any) {
+		const linkGroups = Object.keys(toMap).flatMap((groupName) => {
+			const links = Object.keys(toMap[groupName].links).map((linkTitle) => {
+				const linkJson = toMap[groupName].links[linkTitle]
+				return new Link(linkTitle, linkJson.order, linkJson.customUrl)
+			}).sort((a, b) => {
+				if (a.order === b.order) console.error(`Duplicate order for Link: "${a.title}", "${b.title}: ${a.order}""`)
+				return a.order - b.order
+			})
+			return new LinkGroup(groupName, links, toMap[groupName].order)
+		})
+		return linkGroups.sort((a, b) => {
+			if (a.order === b.order) console.error(`Duplicate order for LinkGroup: "${a.name}", "${b.name}: ${a.order}"`)
+			return a.order - b.order
+		})
+	}
 }
 
 export function isLinkGroupJson(json: LinkJson | LinkGroupJson): json is LinkGroupJson {
-    return (json as LinkGroupJson).links !== undefined
+	return (json as LinkGroupJson).links !== undefined
 }
