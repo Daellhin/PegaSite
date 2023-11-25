0ff2851587c56b36fe438e808bd963dcd14487d6
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2023-11-25 20:59:47 +0100

    Added new records badge

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,7 +2,12 @@
   import { goto } from "$app/navigation"
   import ErrorLine from "$components/ErrorLine.svelte"
   import { authStore } from "$lib/stores/AuthStore"
+  import { clubRecordStore } from "$lib/stores/ClubRecordStore"
   import { pageHeadStore } from "$lib/stores/PageHeadStore"
+
+  const newRecords = $clubRecordStore
+    .flatMap((e) => e.records)
+    .filter((e) => !e.checked).length
 
   // Authguard
   $: authStore.known.then(() => {
@@ -29,8 +34,15 @@
       >
       <a href="/users" class="btn btn-primary normal-case">Gebruikers beheren</a
       >
-	  <a href="/records/edit" class="btn btn-primary normal-case">Clubrecords beheren</a
-		>
+      <a href="/records/edit" class="btn btn-primary normal-case relative"
+        >Clubrecords beheren
+        <div
+          title={`${newRecords} nieuwe clubrecords`}
+          class="absolute -top-2 -end-2 inline-flex items-center justify-center w-6 h-6 text-xs font-bold bg-error border-2 border-base-100 rounded-full"
+        >
+          {newRecords}
+        </div>
+      </a>
     {/if}
   </div>
 {:catch error}
