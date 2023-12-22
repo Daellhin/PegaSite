8b73cb34d81954473bc8e733a3476e731bbe0263
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2023-12-22 22:03:21 +0100

    Migrate to SvelteKit 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static'
 import nesting from 'postcss-nesting'
 import preprocess from 'svelte-preprocess'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
