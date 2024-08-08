09e21a9df0092c383eea2d963db5d55fd24a97c9
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2024-08-08 14:39:21 +0200

    Fixed ShowMore component

--- src/components/ShowMore.svelte
+++ src/components/ShowMore.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
+  export let showMore = false
+  export let startHeightPx = 300
   let classList = ""
   export { classList as class }
 
-  export let showMore = false
-  export let startHeightPx = 300
-
   let container: HTMLElement
+
+  $: contentFits = container?.clientHeight < startHeightPx
 </script>
 
 <div class={"flex flex-col w-fit h-fit " + classList}>
   <div
     class="overflow-hidden"
-    style={`max-height: ${showMore ? "none" : startHeightPx + "px"}`}
+    style={`max-height: ${showMore || contentFits ? "none" : startHeightPx + "px"}`}
   >
     <div bind:this={container}>
       <slot />
@@ -22,7 +23,7 @@
     class:to-base-100={!showMore}
     class:to-transparent={showMore}
   />
-  {#if container?.clientHeight > startHeightPx}
+  {#if !contentFits}
     <button
       class="btn btn-neutral btn-sm mt-3 mx-auto"
       on:click={() => (showMore = !showMore)}
