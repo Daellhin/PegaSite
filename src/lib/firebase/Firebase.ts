7949974f07ef4687fb1c87383def0ce666bd5799
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2024-12-20 20:58:25 +0100

    refactor StorageFolders naming

--- src/lib/firebase/Firebase.ts
+++ src/lib/firebase/Firebase.ts
@@ -22,18 +22,24 @@ export class Collections {
     static readonly PAGES = "pages"
     static readonly USERS = "users"
     static readonly SPONSORS = "sponsors"
-	static readonly ORDERINGS = "orderings"
+    static readonly ORDERINGS = "orderings"
     static readonly PHOTO_ALBUMS = "photo-albums"
     static readonly ANNOUNCEMENTS = "announcements"
 }
 
+export type StorageFolder = {
+    IMAGES: string,
+    THUMBNAILS: string
+}
 /**
  * Firebase Storage folder names
  */
 export class StorageFolders {
     static readonly ARTICLE_IMAGES = "article-images"
-    static readonly PHOTO_ALBUM_IMAGES = "photo-album-images"
-    static readonly PHOTO_ALBUM_THUMBNAILS = "photo-album-thumbnails"
+    static readonly PHOTO_ALBUM: StorageFolder = {
+        IMAGES: "photo-album-images",
+        THUMBNAILS: "photo-album-thumbnails"
+    }
     static readonly SPONSOR_IMAGES = "sponsor-images"
     static readonly PAGE_IMAGES = "page-images"
 }
--- src/lib/utils/UploadProgress.ts
+++ src/lib/utils/UploadProgress.ts
@@ -1,3 +1,12 @@
+import { type StorageFolder } from '$lib/firebase/Firebase'
+import generateImageThumbnail from '$lib/generateImageThumbnail/image-thumbnail'
+import { MAX_CONCURRENT_UPLOADS, WEBP_IMAGE_QUALITY, WEBP_THUMBNAIL_QUALITY } from "$lib/utils/Constants"
+import { updateStoreAtIndex } from "$lib/utils/Svelte"
+import pLimit from 'p-limit'
+import type { Writable } from 'svelte/store'
+import { v4 as uuidv4 } from 'uuid'
+import { blobToWebP } from 'webp-converter-browser'
+
 export enum UploadProgress {
 	NOT_STARTED = 0,
 	CONVERTING = 1,
@@ -12,4 +21,50 @@ export function toString(uploadProgress: UploadProgress) {
 		case UploadProgress.UPLOADING: return "Uploading"
 		case UploadProgress.DONE: return "Done"
 	}
+}
+
+export async function convertAndUploadImages(combinedImages: (string | File)[], folder: StorageFolder, progressStore: Writable<UploadProgress[]>) {
+	const { getStorage, ref, uploadBytes } = await import('firebase/storage')
+
+	const storage = getStorage()
+	const limit = pLimit(MAX_CONCURRENT_UPLOADS)
+	let size = 0
+
+	const uploadedImageIds = await Promise.all(combinedImages.map(async (image, index) => {
+		return limit(async () => {
+			// -- Keep existing url --
+			if (!(image instanceof File)) {
+				updateStoreAtIndex(progressStore, index, UploadProgress.DONE)
+				return image
+			}
+
+			// -- Convert image to webp --
+			updateStoreAtIndex(progressStore, index, UploadProgress.CONVERTING)
+			const convertedImage = await blobToWebP(image, { quality: WEBP_IMAGE_QUALITY })
+			size += convertedImage.size
+
+			// -- Create thumbnail --
+			const thumbnail = await generateImageThumbnail(image, {
+				width: 900,
+				height: 900,
+				maintainAspectRatio: true,
+				type: 'image/webp',
+				quality: WEBP_THUMBNAIL_QUALITY
+			})
+			size += thumbnail.size
+			updateStoreAtIndex(progressStore, index, UploadProgress.UPLOADING)
+
+			// -- Upload --
+			const imageId = uuidv4()
+			const storageRefImages = ref(storage, `${folder.IMAGES}/${imageId}`)
+			const storageRefThumbnails = ref(storage, `${folder.THUMBNAILS}/${imageId}`)
+			await Promise.all([
+				uploadBytes(storageRefImages, convertedImage),
+				uploadBytes(storageRefThumbnails, thumbnail),
+			])
+			updateStoreAtIndex(progressStore, index, UploadProgress.DONE)
+			return imageId
+		})
+	}))
+	return { uploadedImageIds, size }
 }
