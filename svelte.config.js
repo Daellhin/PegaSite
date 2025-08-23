fb9261c3b5e48efcdf08828027bac0cd94fbcaf9
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2025-08-23 20:12:37 +0200

    chorel clean up after upgrade 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,11 @@
 import adapter from '@sveltejs/adapter-static'
-import nesting from 'postcss-nesting'
-import {sveltePreprocess}  from 'svelte-preprocess'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [
-		vitePreprocess(),
-		sveltePreprocess ({
-			postcss: {
-				plugins: [nesting()]
-			}
-		})
-	],
+	preprocess: vitePreprocess(),
 	compilerOptions: {
 		warningFilter: (warning) => {
 			if (
